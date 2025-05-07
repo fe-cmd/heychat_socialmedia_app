e2aa1988c67604bb2cdf458b9cc229cec4a90ded
unknown <oluferonmijoshua@gmail.com>
2025-05-07 00:29:49 -0400

    new commit

--- social/settings.py
+++ social/settings.py
@@ -179,7 +179,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 TEMP = os.path.join(BASE_DIR, 'media_cdn/temp')
